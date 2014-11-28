0b05b1a28623a52fa5e5b0ab11fc02f64104f4d8
Remy Beaumont <remybeaumont@Remys-MacBook-Air.local>
2014-11-28 16:49:53 -0500

    another change

--- build.sh
+++ build.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
 # For centos
-sudo rpm --import "https://pgp.mit.edu/pks/lookup?op=get&search=0x3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF"
+sudo rpm --import https://pgp.mit.edu/pks/lookup?op=get&search=0x3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
 sudo yum-config-manager --add-repo http://download.mono-project.com/repo/centos/
+sudo yum update
 sudo yum install --assumeyes mono-devel 
 
 if test `uname` = Darwin; then
